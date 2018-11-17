fab55668f03b08a54f618a472a8e2ff88bad3380
Vika Denisova <vika.denisova@lebedev-studio.com>
2018-11-17 12:54:18 +0300

    MVP

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,180 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		
+		<b-container>
+			
+			<b-row class="justify-content-md-center">
+				<b-col lg="7" cols="12">
+					<h3>Информация</h3>
+					<div class="info-panel">
+						<b-row>
+							<b-col cols="3">
+								<div class="info-block">
+									<div class="info-block__head">Всего блоков:</div>
+									<div class="info-block__body">{{ this.list.length }}</div>
+								</div>
+							</b-col>
+							<b-col cols="9">
+								<div class="info-block">
+									<div class="info-block__head">Выделенных блоков:</div>
+									<div class="info-block__body">
+										<span>{{ countSelected }}</span>
+										(из них <span class="red">красных: {{ redSelected }},</span>
+										<span class="green">зеленых: {{ greenSelected }}</span>)
+									</div>
+								</div>
+							</b-col>
+						</b-row>
+					</div>
+					
+					<h3>Список блоков</h3>
+					<div class="items-list" v-if="list">
+						<transition-group name="fade" mode="in-out">
+							<text-block v-for="(item, i) in list"
+							            :key="`item_${i}`"
+							            :complex="item.complex"
+							            :index="i">
+								{{ item.text }}
+							</text-block>
+						</transition-group>
+					</div>
+					
+					<div class="add-item">
+						<b-button variant="primary" @click="addItem">Добавить блок</b-button>
+					</div>
+				</b-col>
+			</b-row>
+		
+		</b-container>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+	import TextBlock  from './components/TextBlock'
+	import loremIpsum from 'lorem-ipsum'
+	
+	export default {
+		name: 'app',
+		components: {
+			TextBlock
+		},
+		data () {
+			return {
+				list: null,
+				countSelected: 0,
+				redSelected: 0,
+				greenSelected: 0
+			}
+		},
+		
+		created () {
+			this.generateList()
+			
+			this.$root.$on('removeTextBlock', (index) => {
+				this.list.splice(index, 1)
+			})
+			
+			this.$root.$on('selectBlock', (item) => {
+				console.log(item)
+				this.countSelected += item.selected ? 1 : -1
+				
+				if (item.selected && item.complex) {
+					this.redSelected += item.color === 'red' ? 1 : 0
+					this.greenSelected += item.color === 'green' ? 1 : 0
+				} else if (!item.selected && item.complex) {
+					this.redSelected += item.color === 'red' ? -1 : 0
+					this.greenSelected += item.color === 'green' ? -1 : 0
+				}
+			})
+		},
+		
+		beforeDestroy () {
+			this.$root.$off('removeTextBlock')
+		},
+		
+		methods: {
+			generateList () {
+				this.list = Array.from({length: 5})
+				
+				for (let i in this.list) {
+					this.list[i] = this.generateItem()
+				}
+			},
+			
+			generateItem () {
+				let obj = {}
+				
+				obj.complex = !Math.round(Math.random())
+				obj.text = loremIpsum({count: 4})
+				
+				return obj
+			},
+			
+			addItem () {
+				this.list.push(this.generateItem())
+			}
+		}
+	}
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+		margin-bottom: 40px;
+		
+		h3 {
+			margin-bottom: 10px;
+			text-align: left;
+		}
+	}
+	
+	.info-panel {
+		margin-bottom: 40px;
+		background-color: #eaeaea;
+		border-radius: 5px;
+		padding: 20px;
+	}
+	
+	.items-list {
+		margin-bottom: 40px;
+	}
+	
+	.info-block {
+		text-align: left;
+		&__head {
+			font-size: 16px;
+			margin-bottom: 8px;
+		}
+		&__body {
+			span {
+				margin-right: 20px;
+				
+				&:last-child {
+					margin-right: 0;
+				}
+			}
+		}
+	}
+	
+	.red {
+		color: #f57474;
+	}
+	
+	.green {
+		color: #16b316;
+	}
+	
+	.fade-enter-active,
+	.fade-leave-active {
+		transition: opacity .5s;
+	}
+	
+	.fade-enter, .fade-leave-to {
+		opacity: 0;
+	}
 </style>
---
+++ src/components/TextBlock.vue
@@ -0,0 +1,166 @@
+<template>
+	<div :class="['text-block', {'is-selected': selected}, complexClass]">
+		<div class="text-block__close" @click="removeItem"></div>
+		<div class="text-block__id">#{{ index }}</div>
+		<div class="text-block__content" @click="clickHandler" @dblclick="dblClickHandler">
+			<slot/>
+		</div>
+		
+		<b-modal v-model="modalShow"
+		         cancel-title="Отмена"
+		         ok-title="Да"
+		         @ok="confirmRemoving">
+			Удалить блок?
+		</b-modal>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'TextBlock',
+		props: {
+			/**
+			 * Enables/disables complex block display
+			 * @type {Boolean}
+			 */
+			complex: {
+				type: Boolean,
+				default: false
+			},
+			
+			index: {
+				type: Number
+			}
+		},
+		
+		data () {
+			return {
+				selected: false,
+				dbClick: false,
+				modalShow: false,
+				timer: 0,
+				delay: 200,
+				prevent: false
+			}
+		},
+		
+		computed: {
+			complexClass () {
+				if (this.complex) {
+					return this.dbClick ? 'text-block--green' : 'text-block--red'
+				}
+			}
+		},
+		
+		methods: {
+			removeItem () {
+				if (this.complex) {
+					this.modalShow = true
+					return
+				}
+				
+				this.selectItem(false)
+				this.$root.$emit('removeTextBlock', this.index)
+			},
+			
+			clickHandler () {
+				let self = this
+				
+				this.timer = setTimeout(() => {
+					if (!self.prevent) {
+						self.selectItem(!self.selected)
+					}
+					self.prevent = false
+				}, self.delay)
+			},
+			
+			dblClickHandler () {
+				clearTimeout(this.timer)
+				this.prevent = true
+				this.dbClick = !this.dbClick
+			},
+			
+			selectItem (value) {
+				this.selected = value
+				
+				this.$root.$emit('selectBlock', {
+					selected: this.selected,
+					complex: this.complex,
+					color: this.complex ? (this.dbClick ? 'green' : 'red') : false
+				})
+			},
+			
+			confirmRemoving () {
+				this.selectItem(false)
+				this.$root.$emit('removeTextBlock', this.index)
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	.text-block {
+		position: relative;
+		background-color: white;
+		border-radius: 5px;
+		margin-bottom: 16px;
+		transition: background-color 0.3s ease;
+		
+		&:before {
+			content: '';
+			position: absolute;
+			z-index: 0;
+			width: 100%;
+			height: 100%;
+			top: 0;
+			left: 0;
+			border: 1px solid gainsboro;
+			border-radius: 5px;
+			transition: border 0.3s ease;
+		}
+		
+		&:last-child {
+			margin-bottom: 0;
+		}
+		
+		&__close {
+			position: absolute;
+			z-index: 1;
+			top: 10px;
+			right: 10px;
+			width: 32px;
+			height: 32px;
+			background: url('../assets/cancel.svg') no-repeat center/14px 14px;
+			opacity: 0.3;
+			cursor: pointer;
+			transition: opacity 0.3s ease;
+			
+			&:hover {
+				opacity: 0.5;
+			}
+		}
+		
+		&__content {
+			position: relative;
+			padding: 20px 40px 20px 20px;
+			font-size: 16px;
+			text-align: left;
+			user-select: none;
+		}
+		
+		&.is-selected {
+			&:before {
+				border: 3px solid #6499ff;
+			}
+			
+		}
+		
+		&--red {
+			background-color: #ffe7e7;
+		}
+		
+		&--green {
+			background-color: #caffce;
+		}
+	}
+</style>
